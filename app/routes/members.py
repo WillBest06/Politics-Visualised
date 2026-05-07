661915d4fe96dd31b401dcff37f64f284eeb17ba
William Best <148398824+WillBest06@users.noreply.github.com>
2026-05-07 13:02:15 +0100

    add pagination and minor UI tweaks

--- app/routes/members.py
+++ app/routes/members.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, session, request
 import requests
+import math
 
 members_bp = Blueprint("members", __name__)
-
-@members_bp.route('/')
-def options():
-    return render_template('members.html')
 
 @members_bp.route('/search', methods=['GET'])
 def search():
@@ -14,24 +11,29 @@ def search():
     
     search_type = request.args.get('t') 
     query = request.args.get('q')
+    
+    page = request.args.get('page', 1, type=int)
 
     if query:
         query = query.strip()
-        api_url = f'https://members-api.parliament.uk/api/Members/Search?{search_type}={query}&skip=0&take=20'
+        take = 10  
+        skip = (page - 1) * take 
+        
+        api_url = f'https://members-api.parliament.uk/api/Members/Search?{search_type}={query}&skip={skip}&take={take}'
         r = requests.get(api_url)
         
         if r.status_code == 200:
             data = r.json()
             members_data = data.get('items', [])
             total_results = data.get('totalResults', 0)
-            result_context = data.get('resultContext', 0)
+            
+            total_pages = math.ceil(total_results / take)
             
             return render_template('members/results.html', 
                                    form=form, 
                                    members=members_data, 
                                    totalResults=total_results,
-                                   resultContext=result_context,
-                                   query=query
-                                   )
+                                   current_page=page,
+                                   total_pages=total_pages)
 
     return render_template('members/search.html', form=form)
