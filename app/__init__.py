ef807e4a80b9a7732ea94ec798ce3563484c50ae
William Best <148398824+WillBest06@users.noreply.github.com>
2026-05-08 12:19:11 +0100

    add environment secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
+import os
+
 def create_app(test_config=None):
     app = Flask(__name__)
     
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'local-dev-only-key'),
+        
+        SESSION_TYPE='filesystem',
+        SESSION_FILE_DIR=os.path.join(app.root_path, 'flask_session_data'),
+        SESSION_PERMANENT=False,
+
+        MAX_CONTENT_LENGTH=5 * 1024 * 1024
     )
 
     csrf = CSRFProtect(app) 
